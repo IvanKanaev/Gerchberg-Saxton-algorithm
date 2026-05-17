cded63c5721fca8dbc1f9e806f52438517dfa7b4
EvdokimovAN <evdokimov.an@phystech.edu>
2026-05-17 17:33:34 +0300

    Добавлен хедор для основного класса tenzor

---
+++ include/tensor.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <complex>
+
+class Tensor{
+protected:
+    std::vector<std::vector<std::vector<double>>> data;
+    std::vector<double> z_distance;//расположение плоскостей
+    double lam;//длина волны
+    std::vector<double> fx;//пространственные частоты(считаем сразу в конструкторе)
+    std::vector<double> fy;
+    double dx;    //размер пикселя по x
+    double dy;    //размер пикселя по y
+public:
+    std::vector<std::vector<double>> amplitude;
+    std::vector<std::vector<double>> phase;
+    std::vector<double> errors;
+
+
+
+    Tensor(const std::vector<std::string>& path, const std::vector<double> z_distance, double dx, double dy);
+    // Конструктор принимает вектор с путями на все файлы
+    // Заполняет тензор интенсивностями
+    // Заполняет fx, fy, z
+
+
+    std::vector<std::vector<std::complex<double>>> peredat_func(int i);
+    // Считает передаточную функцию между i-той и i+1 плоскостью
+
+
+    
+    std::vector<std::vector<std::complex<double>>> gerchberg_saxton(int iterations = 100,double tolerance = 1e-4);
+    //агоритм gerchberg_saxton {возвращает комплексное поле в 0 плоскости, нужно ли это???}
+    // заполняет поля рассчитаными алгоритмом данными amplitude; phase;(в 0 плоскости) errors;
+
+
+};
